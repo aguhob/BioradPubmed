75eb82812e019ebd399506fba77fc526271b084c
Minda Aguhob <ma@peakfoqus.com>
2018-05-22 05:33:34 +0000

    Init to end of part 5

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Materials1' },
+  { id: 12, name: 'Materials2' },
+  { id: 13, name: 'Methods3' },
+  { id: 14, name: 'Methods4' },
+  { id: 15, name: 'Biochem5' },
+  { id: 16, name: 'Biochem6' }
+];
