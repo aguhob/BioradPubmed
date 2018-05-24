07c7864a677927de88a233914e868584c4a54a53
Minda Aguhob <ma@peakfoqus.com>
2018-05-24 23:39:44 +0000

    Part 6. Get heroes with HttpClientb

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -13,11 +13,16 @@ export class HeroService {
   
   constructor(private http: HttpClient, private messageService: MessageService) { 
   }
-  
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+
+  // getHeroes(): Observable<Hero[]> {
+  //   // TODO: send the message _after_ fetching the heroes
+  //   this.messageService.add('HeroService: fetched heroes');
+  //   return of(HEROES);
+  // }
+
+  /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
